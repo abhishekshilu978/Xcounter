a8fef94a9523ae046a18f3fa5b025867251efd89
abhishekshilu978 <abhishekkumar1087@gmail.com>
2025-11-01 15:31:34 +0530

    3rd changes

---
+++ vite-project/src/reportWebVitals.jsx
@@ -0,0 +1,15 @@
+import App from './App';
+
+const reportWebVitals = onPerfEntry => {
+    if(onPerfEntry && onPerfEntry instanceof Function) {
+        import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+            getCLS(onPerfEntry)
+            getFCP(onPerfEntry)
+            getFID(onPerfEntry)
+            getLCP(onPerfEntry)
+            getTTFB(onPerfEntry)
+
+        });
+    }
+}
+export default reportWebVitals;
